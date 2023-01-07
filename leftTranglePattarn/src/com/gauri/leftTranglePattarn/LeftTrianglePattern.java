56070ae3a781ae6c200ece566da72bac01949cca
Gauri Ragade <gauriragade98@gmail.com>
2023-01-07 14:35:15 +0530

    new change in row

--- leftTranglePattarn/src/com/gauri/leftTranglePattarn/LeftTrianglePattern.java
+++ leftTranglePattarn/src/com/gauri/leftTranglePattarn/LeftTrianglePattern.java
@@ -3,11 +3,9 @@ package com.gauri.leftTranglePattarn;
 public class LeftTrianglePattern {
 	public static void main(String args[]) {
 
-		int i, j, row = 6;
-
+		int i, j, row = 10;
 		for (i = 0; i < row; i++) {
-
-			for (j = 2*(row-1); j >= 0; j--) {
+			for (j = 2 * (row - 1); j >= 0; j--) {
 
 				System.out.print("");
 			}
